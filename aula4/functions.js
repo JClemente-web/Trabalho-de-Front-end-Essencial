1aa6fe5c2f8dc8c454ec8c93aba0affaf180080b
JClemente-web <joaovitorclementeferreira70@gmail.com>
2026-05-03 16:07:23 -0300

    exercicios avaliacao individual

---
+++ aula4/functions.js
@@ -0,0 +1,8 @@
+function somar(a,b){
+    return a + b;
+}
+
+
+console.log(somar(2,18));
+
+const subtrair = (b,c) => b - c;
---
+++ aula4/variables.js
@@ -0,0 +1,14 @@
+var nome = "Maria";
+console.log(nome);
+
+nome = "João";
+console.log(nome);
+
+function printPhone() {
+    let phone = "123456789";
+    phpne ='000000000'
+    console.log(phone);
+
+}
+
+printPhone();
